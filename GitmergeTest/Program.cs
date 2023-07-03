583607af48ac33a85c7f67c9756a5e7fc1568c75
CVA11Ha11A <dad0221@naver.com>
2023-07-03 16:16:34 +0900

    Add.UserInput //beta

--- GitmergeTest/Program.cs
+++ GitmergeTest/Program.cs
@@ -8,34 +8,44 @@ namespace GitmergeTest
 {
     public class Program
     {
+            //{     2023. 07.03. Add user input /beta   
         static void Main(string[] args)
         {
-            Ruler ruler = new Ruler(10);
+            string userInput = string.Empty;
+            Console.WriteLine("This program convert cm to Inch");
+            Console.WriteLine("Input Cm value : "); ;
+            userInput = Console.ReadLine();
+
+            int cmInput = 0;
+            int.TryParse(userInput, out cmInput);
+
+            Ruler ruler = new Ruler(cmInput);
+            //}     2023. 07.03. Add user input /beta   
             ruler.Run();
 
 
 
         }
     }
-        public class Ruler
+    public class Ruler
+    {
+        private const float ONE_INCH = 2.54F;
+
+        public int Centimeter { get; set; } = 0;
+
+        public float Inch
         {
-            private const float ONE_INCH = 2.54F;
-
-            public int Centimeter { get; set; } = 0;
-
-            public float Inch
-            {
-                get { return Centimeter * ONE_INCH; }
-                private set { Centimeter = (int)(value / ONE_INCH); }
-            }
-
-            public Ruler(int cmValue) { Centimeter = cmValue; }
-
-            public void Run()
-            {
-                Console.WriteLine($"{this.Centimeter}cm 는 {this.Inch}inch 입니다.");
-
-            }
+            get { return Centimeter * ONE_INCH; }
+            private set { Centimeter = (int)(value / ONE_INCH); }
         }
-    
+
+        public Ruler(int cmValue) { Centimeter = cmValue; }
+
+        public void Run()
+        {
+            Console.WriteLine($"{this.Centimeter}cm 는 {this.Inch}inch 입니다.");
+
+        }
+    }
+
 }
